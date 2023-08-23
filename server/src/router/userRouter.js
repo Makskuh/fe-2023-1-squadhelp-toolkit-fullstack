031c7b533063dbec95fcd04c02d2caa2560a6a31
Max Kuh <maxes2805582@gmail.com>
2023-08-23 19:50:35 +0300

    Refactor userRouter

---
+++ server/src/router/userRouter.js
@@ -0,0 +1,43 @@
+const userRouter = require('express').Router();
+const userController = require('../controllers/userController');
+const checkToken = require('../middlewares/checkToken');
+const basicMiddlewares = require('../middlewares/basicMiddlewares');
+const upload = require('../utils/fileUpload');
+const validators = require('../middlewares/validators');
+userRouter.post(
+  '/getUser',
+  checkToken.checkAuth,
+);
+
+userRouter.post(
+  '/changeMark',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCustomer,
+  userController.changeMark,
+);
+
+userRouter.post(
+  '/updateUser',
+  checkToken.checkToken,
+  upload.uploadAvatar,
+  userController.updateUser,
+);
+
+userRouter.post(
+  '/cashout',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCreative,
+  userController.cashout,
+);
+
+userRouter.post(
+  '/pay',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCustomer,
+  upload.uploadContestFiles,
+  basicMiddlewares.parseBody,
+  validators.validateContestCreation,
+  userController.payment,
+);
+
+module.exports = userRouter;
