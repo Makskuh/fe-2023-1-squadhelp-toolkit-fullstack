58afd9b359efbe5ae19cb9a99765522ecebb18ee
Max Kuh <maxes2805582@gmail.com>
2023-08-25 14:17:22 +0300

    Add path to main page

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -116,11 +116,13 @@ class Header extends React.Component {
           </div>
         </div>
         <div className={styles.navContainer}>
+          <a href='/'>
           <img
             src={`${CONSTANTS.STATIC_IMAGES_PATH}blue-logo.png`}
             className={styles.logo}
             alt="blue_logo"
           />
+          </a>
           <div className={styles.leftNav}>
             <div className={styles.nav}>
               <ul>
