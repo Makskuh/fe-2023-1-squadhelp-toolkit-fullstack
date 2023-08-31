f1bd9f61423b8e56741e235fb00d06b871518702
Max Kuh <maxes2805582@gmail.com>
2023-08-31 16:32:24 +0300

    Add 2 components

---
+++ client/src/components/HowItWork components/NamingContestWork/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function NamingContestWork(props) {
+  return (
+    <div>NamingContestWork
+    </div>
+  )
+}
+
+export default NamingContestWork
---
+++ client/src/components/HowItWork components/WaysToUseSquadhelp/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function WaysToUseSquadhelp(props) {
+  return <div>WaysToUseSquadhelp props </div>;
+}
+
+export default WaysToUseSquadhelp;
