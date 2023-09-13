fa84e091b9f31321a375701413f45c855f110a46
Max Kuh <maxes2805582@gmail.com>
2023-09-13 21:20:13 +0300

    Add img and styles for title

--- client/src/components/TimerEvents/EventsList/index.jsx
+++ client/src/components/TimerEvents/EventsList/index.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styles from './EventsList.module.sass';
-import { connect, useDispatch } from 'react-redux';
-import { deleteEvent, updateEvent } from '../../../store/slices/eventsSlice';
+import { connect } from 'react-redux';
 import EventItem from '../EventItem';
+import constants from '../../../constants';
 function EventsList({ events }) {
   return (
     <div className={styles.ListWrapper}>
       <div className={styles.titleWrapper}>
-        <h1 className={styles.title}>Live upcomming checks</h1>
-        <p className={styles.remainingTime}>
-          Remaining time
-        </p>
+        <h1 className={styles.title}>Event Timer</h1>
+        <div className={styles.clockWrapper}>
+          <img src={`${constants.STATIC_IMAGES_PATH}howitworks/timer.png`} alt="" />
+          <p className={styles.remainingTime}>Remaining time</p>
+        </div>
       </div>
       <ul className={styles.ulEvents}>
         {events.map((event) => (
-          <EventItem event={event} key={event.id}/>
+          <EventItem event={event} key={event.id} />
         ))}
       </ul>
     </div>
