8e536c5d77eb55de134858059ab7111e722b818a
Max Kuh <maxes2805582@gmail.com>
2023-09-20 22:19:03 +0300

    Add table messages

--- server/tasks-ProjectDB/postgresTask.sql
+++ server/tasks-ProjectDB/postgresTask.sql
@@ -1,8 +1,21 @@
-
-
 CREATE TABLE messages (
-  ID SERIAL PRIMARY KEY ,
-  BODY VARCHAR(1000)
-  conversation INT FOREIGN KEY REFERENCES conversation(id)
-  sender INT FOREIGN KEY REFERENCES Users(id)
+  id SERIAL PRIMARY KEY,
+  body TEXT,
+  sender INT REFERENCES "Users"
 )
+ALTER TABLE messages
+ADD COLUMN "dialogue" INT REFERENCES "conversation",
+ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp;
+
+CREATE TABLE "conversation" (
+  id SERIAL PRIMARY KEY,
+  participants NUMBER NOT NULL ,
+  blacklist
+)
+
+
+
+
+
+SELECT  * FROM messages
