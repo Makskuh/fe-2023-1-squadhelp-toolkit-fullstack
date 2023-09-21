35ac5b83be9a200f30c940dcb39ddfa53c539b18
Max Kuh <maxes2805582@gmail.com>
2023-09-21 15:47:45 +0300

    Add solution postgressql db chat

--- server/tasks-ProjectDB/postgresTask.sql
+++ server/tasks-ProjectDB/postgresTask.sql
@@ -1,21 +1,27 @@
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY,
   body TEXT,
-  sender INT REFERENCES "Users"
+  sender INT REFERENCES "Users"(id),
+  "conversation_id" INT REFERENCES "conversation"(id),
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 )
-ALTER TABLE messages
-ADD COLUMN "dialogue" INT REFERENCES "conversation",
-ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp;
+
 
 CREATE TABLE "conversation" (
   id SERIAL PRIMARY KEY,
-  participants NUMBER NOT NULL ,
-  blacklist
+  participants INT[] NOT NULL,
+  blacklist BOOLEAN[] NOT NULL,
+  favoritelist BOOLEAN[]  NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 )
 
-
-
-
+CREATE TABLE "Catalog" (
+  id SERIAL PRIMARY KEY,
+  "catalogName" TEXT,
+  "userId" INT REFERENCES "Users"(id),
+  "conversation_id" INT REFERENCES "conversation"(id)
+)
 
 SELECT  * FROM messages
