3fb1fd9196307815c267c672a184f68f598571f4
Max Kuh <maxes2805582@gmail.com>
2023-10-17 15:46:41 +0300

    Add a second solution for the logger

--- server/src/handlerError/logger/loggerUpdater.js
+++ server/src/handlerError/logger/loggerUpdater.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 module.exports.loggerUpdater = (err, res, req, next) => {
   const logDir = './log';
-  const mainLog = fs.readFileSync(`${logDir}/error.log`, {
-    encoding: 'utf8',
-    flag: 'r',
-  });
-  console.log(mainLog)
+  const logArray = []
+  // const mainLog = fs.readFileSync(`${logDir}/error.json`, {
+  //   encoding: 'utf8',
+  //   flag: 'r',
+  // });
   next();
 };
--- server/src/handlerError/logger/writeToFileLog.js
+++ server/src/handlerError/logger/writeToFileLog.js
@@ -1,14 +1,28 @@
 const fs = require('fs');
 
 module.exports.writeToFile = (err) => {
-    const logDir = './log';
-    const data = `{message: "${err.message}",timestamp: ${new Date().getTime()}, code: ${err.code}, stackTrace: {${err.stack}}},\r\n`;
-    if (!fs.existsSync(logDir)) {
-      fs.mkdirSync(logDir);
-    }
-    const options = {
-      encoding: 'utf8',
-      code: 438,
-    };
-    return fs.appendFileSync(`${logDir}/error.log`, data, options);
+  const logDir = './log';
+  let arrayLogError = [];
+  const options = {
+    encoding: 'utf8',
+    code: 438,
+  };
+  const data = {
+    message: err.message,
+    timestamp: new Date().getTime(),
+    code: err.code,
+    stackTrace: err.stack,
+  };
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+    arrayLogError.push(data);
+    const arrJSON = JSON.stringify(arrayLogError);
+    return fs.appendFileSync(`${logDir}/error.json`, arrJSON, options);
+  }
+  let lastLogToJson = JSON.parse(fs.readFileSync(`${logDir}/error.json`, {
+    encoding: 'utf8',
+    flag: 'r',
+  }));
+  lastLogToJson.push(data)
+  return fs.writeFileSync(`${logDir}/error.json`, JSON.stringify(lastLogToJson), options);
 };
